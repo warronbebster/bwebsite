960e780e79db746602f83b3ccc649e82184586e5
Barron Webster <visualwebster@gmail.com>
2020-05-10 16:51:12 -0400

    got svgs working with arbitrary vector shapes

--- nodeFigma.js
+++ nodeFigma.js
@@ -23,7 +23,7 @@ const baseUrl = 'https://api.figma.com';
 
 const vectorMap = {};
 const vectorList = [];
-const vectorTypes = ['VECTOR', 'LINE', 'REGULAR_POLYGON', 'ELLIPSE', 'STAR'];
+const vectorTypes = ['VECTOR', 'LINE', 'REGULAR_POLYGON', 'STAR'];
 
 //shit begins after this
 //shit begins after this
@@ -31,83 +31,83 @@ const vectorTypes = ['VECTOR', 'LINE', 'REGULAR_POLYGON', 'ELLIPSE', 'STAR'];
 
 //DON'T DELETE
 //DON'T DELETE
-// function preprocessTree(node) {
-// 	//this code runs once per frame with #
-// 	let vectorsOnly = node.name.charAt(0) !== '#'; //things that don't start with #
-// 	let vectorVConstraint = null;
-// 	let vectorHConstraint = null;
+function preprocessTree(node) {
+	//this code runs once per frame with #
+	let vectorsOnly = node.name.charAt(0) !== '#'; //things that don't start with #
+	let vectorVConstraint = null;
+	let vectorHConstraint = null;
 
-// 	//this function just returns true if paints exist
-// 	function paintsRequireRender(paints) {
-// 		if (!paints) return false; //if no paints
-// 		let numPaints = 0;
-// 		for (const paint of paints) {
-// 			if (paint.visible === false) continue; //jump this section of loop
-// 			numPaints++;
-// 			if (paint.type === 'EMOJI') return true; //wat
-// 		}
-// 		return numPaints > 1;
-// 	}
+	//this function just returns true if paints exist
+	function paintsRequireRender(paints) {
+		if (!paints) return false; //if no paints
+		let numPaints = 0;
+		for (const paint of paints) {
+			if (paint.visible === false) continue; //jump this section of loop
+			numPaints++;
+			if (paint.type === 'EMOJI') return true; //wat
+		}
+		return numPaints > 1;
+	}
 
-// 	if (
-// 		//if paints exist, fill or stroke
-// 		paintsRequireRender(node.fills) ||
-// 		paintsRequireRender(node.strokes) ||
-// 		(node.blendMode != null &&
-// 			['PASS_THROUGH', 'NORMAL'].indexOf(node.blendMode) < 0)
-// 	) {
-// 		node.type = 'VECTOR'; //set node type to vector?
-// 	}
+	if (
+		//if paints exist, fill or stroke
+		paintsRequireRender(node.fills) ||
+		paintsRequireRender(node.strokes) ||
+		(node.blendMode != null &&
+			['PASS_THROUGH', 'NORMAL'].indexOf(node.blendMode) < 0)
+	) {
+		// node.type = 'VECTOR'; //set node type to vector?
+	}
 
-// 	const children =
-// 		node.children && node.children.filter((child) => child.visible !== false);
-// 	if (children) {
-// 		//if there are children in the node
-// 		for (let j = 0; j < children.length; j++) {
-// 			if (vectorTypes.indexOf(children[j].type) < 0) {
-// 				//if it's not one of the types listed above
-// 				vectorsOnly = false; //set no vectors?
-// 			} else {
-// 				if (
-// 					vectorVConstraint != null &&
-// 					children[j].constraints.vertical != vectorVConstraint
-// 				)
-// 					vectorsOnly = false;
-// 				if (
-// 					vectorHConstraint != null &&
-// 					children[j].constraints.horizontal != vectorHConstraint
-// 				)
-// 					vectorsOnly = false;
-// 				vectorVConstraint = children[j].constraints.vertical;
-// 				vectorHConstraint = children[j].constraints.horizontal;
-// 			}
-// 		}
-// 	}
-// 	node.children = children; //rewrite node.children with new thing
+	const children =
+		node.children && node.children.filter((child) => child.visible !== false);
+	if (children) {
+		//if there are children in the node
+		for (let j = 0; j < children.length; j++) {
+			if (vectorTypes.indexOf(children[j].type) < 0) {
+				//if it's not one of the types listed above
+				vectorsOnly = false; //set no vectors?
+			} else {
+				if (
+					vectorVConstraint != null &&
+					children[j].constraints.vertical != vectorVConstraint
+				)
+					vectorsOnly = false;
+				if (
+					vectorHConstraint != null &&
+					children[j].constraints.horizontal != vectorHConstraint
+				)
+					vectorsOnly = false;
+				vectorVConstraint = children[j].constraints.vertical;
+				vectorHConstraint = children[j].constraints.horizontal;
+			}
+		}
+	}
+	node.children = children; //rewrite node.children with new thing
 
-// 	if (children && children.length > 0 && vectorsOnly) {
-// 		//if there are children, and "vectorsOnly"
-// 		node.type = 'VECTOR';
-// 		node.constraints = {
-// 			vertical: vectorVConstraint,
-// 			horizontal: vectorHConstraint,
-// 		};
-// 	}
+	if (children && children.length > 0 && vectorsOnly) {
+		//if there are children, and "vectorsOnly"
+		// node.type = 'VECTOR';
+		node.constraints = {
+			vertical: vectorVConstraint,
+			horizontal: vectorHConstraint,
+		};
+	}
 
-// 	if (vectorTypes.indexOf(node.type) >= 0) {
-// 		node.type = 'VECTOR';
-// 		vectorMap[node.id] = node;
-// 		vectorList.push(node.id); //adds to vectorlist
-// 		node.children = [];
-// 	}
+	if (vectorTypes.indexOf(node.type) >= 0) {
+		// node.type = 'VECTOR';
+		vectorMap[node.id] = node;
+		vectorList.push(node.id); //adds to vectorlist
+		node.children = [];
+	}
 
-// 	if (node.children) {
-// 		//if there are children
-// 		for (const child of node.children) {
-// 			preprocessTree(child); //oh it's recursive baybee
-// 		}
-// 	}
-// }
+	if (node.children) {
+		//if there are children
+		for (const child of node.children) {
+			preprocessTree(child); //oh it's recursive baybee
+		}
+	}
+}
 //DON'T DELETE
 //DON'T DELETE
 
@@ -131,14 +131,16 @@ async function main() {
 
 	//DON'T DELETE
 	//DON'T DELETE
-	// for (let i = 0; i < canvas.children.length; i++) {
-	// 	//for each child  of canvas
-	// 	const child = canvas.children[i];
-	// 	if (child.name.charAt(0) === '#' && child.visible !== false) {
-	// 		preprocessTree(child); //run preprocess function?
-	// 		//for each frame
-	// 	}
-	// }
+
+	for (let i = 0; i < canvas.children.length; i++) {
+		//for each child  of canvas
+		const child = canvas.children[i];
+		if (child.name.charAt(0) === '#' && child.visible !== false) {
+			preprocessTree(child); //run preprocess function?
+			//for each frame
+		}
+	}
+
 	//okay soooo it's unclear to me what "preprocess" does…
 	//maybe it cleans up files if there's a bunch of extra stuff?
 	//or stuff the system isn't expecting?
@@ -149,21 +151,28 @@ async function main() {
 	//does it get populated by preprocess?
 	//GU IDS ??
 	let guids = vectorList.join(','); //this grabs the vectors?
-	data = await fetch(
+	debugger;
+
+	let svgData = await fetch(
 		`${baseUrl}/v1/images/${fileKey}?ids=${guids}&format=svg`,
 		{ headers }
-	); //grabs svgs of vectorList
-	const imageJSON = await data.json(); //makes json out of them
+	);
+	debugger;
 
-	const images = imageJSON.images || {}; //if images exist?
-	if (images) {
+	//grabs svgs of vectorList
+	const svgJSON = await svgData.json(); //makes json out of them
+
+	debugger;
+
+	const svgs = svgJSON.images || {}; //if images exist?
+	if (svgs) {
 		//if images exist, process like this?
 		let promises = [];
 		let guids = [];
-		for (const guid in images) {
-			if (images[guid] == null) continue;
+		for (const guid in svgs) {
+			if (svgs[guid] == null) continue;
 			guids.push(guid);
-			promises.push(fetch(images[guid]));
+			promises.push(fetch(svgs[guid]));
 		}
 
 		let responses = await Promise.all(promises);
@@ -175,7 +184,7 @@ async function main() {
 		responses = await Promise.all(promises);
 		for (let i = 0; i < responses.length; i++) {
 			//replace svg with svg preserveAspectRatio none
-			images[guids[i]] = responses[i].replace(
+			svgs[guids[i]] = responses[i].replace(
 				'<svg ',
 				'<svg preserveAspectRatio="none" '
 			);
@@ -191,7 +200,7 @@ async function main() {
 		const child = canvas.children[i]; //child variable
 		if (child.name.charAt(0) === '#' && child.visible !== false) {
 			//if named & visible
-			figma.createComponent(child, images, imageFills, componentMap);
+			figma.createComponent(child, svgs, imageFills, componentMap);
 			//hit figma lib
 			//pass the frame and images?
 			//returns object & updates componentMap object
